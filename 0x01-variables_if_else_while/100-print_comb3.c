ede006d3021df6a2014693c2c76eb4c91ffd099c
believe19 <olajideadebowalejai@gmail.com>
2023-02-17 13:21:51 -0800

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main access
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		if (!(i > j || i == j))
+		{
+			putchar(i);
+			putchar(j);
+			if (i == '8' && j == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		j++;
+		i++;
+	}
+	return (0);
+}
