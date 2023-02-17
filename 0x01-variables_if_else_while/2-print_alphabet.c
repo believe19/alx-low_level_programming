4801547fd5ef9bfa1a6f214e3204ee7a00c20766
believe19 <olajideadebowalejai@gmail.com>
2023-02-17 08:41:13 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main access
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
