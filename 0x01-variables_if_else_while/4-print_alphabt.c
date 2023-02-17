a3eb9211cb43b54679be0ea685aee0f789045845
believe19 <olajideadebowalejai@gmail.com>
2023-02-17 09:12:18 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main access
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
