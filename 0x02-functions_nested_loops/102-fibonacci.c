ef5a8c3a56d3e48afab2158e2537f20943c955ad
believe19 <olajideadebowalejai@gmail.com>
2023-02-22 03:30:01 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,14 +10,13 @@ int main(void)
 {
 	int x = 0;
 	long y = 1, z = 2;
-	
-	
+
 	while (x < 50)
 	{
 		if (x == 0)
-			printf("%ld", y);
+		printf("%ld", y);
 		else if (x == 1)
-		       printf("%ld," z);
+		printf(",%ld", z);
 		else
 		{
 			z += y;
@@ -29,4 +28,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
